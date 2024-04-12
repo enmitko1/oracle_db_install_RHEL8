33ab4f7bef1391e37af1bcec00cba49e34e178c0
Dimitar <78085216+enmitko1@users.noreply.github.com>
2024-04-12 19:10:58 +0200

    Create install_software_ora19c.sh

---
+++ install_software_ora19c.sh
@@ -0,0 +1,35 @@
+cd $ORACLE_HOME
+
+echo "Starting unzipping of the software
+"
+unzip -oq /home/oracle/LINUX.X64_193000_db_home.zip
+
+echo "Finished unzipping
+"
+
+# Fake Oracle Linux 7.
+export CV_ASSUME_DISTID=OEL7.6
+
+echo "Starting software installation
+"
+# Silent mode.
+./runInstaller -ignorePrereq -waitforcompletion -silent                        \
+    -responseFile ${ORACLE_HOME}/install/response/db_install.rsp               \
+    oracle.install.option=INSTALL_DB_SWONLY                                    \
+    ORACLE_HOSTNAME=${ORACLE_HOSTNAME}                                         \
+    UNIX_GROUP_NAME=oinstall                                                   \
+    INVENTORY_LOCATION=${ORA_INVENTORY}                                        \
+    SELECTED_LANGUAGES=en,en_US                                                \
+    ORACLE_HOME=${ORACLE_HOME}                                                 \
+    ORACLE_BASE=${ORACLE_BASE}                                                 \
+    oracle.install.db.InstallEdition=EE                                        \
+    oracle.install.db.OSDBA_GROUP=dba                                          \
+    oracle.install.db.OSBACKUPDBA_GROUP=dba                                    \
+    oracle.install.db.OSDGDBA_GROUP=dba                                        \
+    oracle.install.db.OSKMDBA_GROUP=dba                                        \
+    oracle.install.db.OSRACDBA_GROUP=dba                                       \
+    SECURITY_UPDATES_VIA_MYORACLESUPPORT=false                                 \
+    DECLINE_SECURITY_UPDATES=true
+
+echo "Finished installing Oracle 19c software
+"
