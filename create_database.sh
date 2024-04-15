b02fbeb3c6f6f9b9714a828208595499a9aed319
enmitko1 <enmitko1@gmail.com>
2024-04-15 13:41:00 +0200

    Creado el script create_database.sh que se quedó sin añadir al repositorio.

---
+++ create_database.sh
@@ -0,0 +1,24 @@
+echo "Starting database creation
+"
+
+dbca -silent -createDatabase                                                   \
+     -templateName General_Purpose.dbc                                         \
+     -gdbname ${ORACLE_SID} -sid  ${ORACLE_SID} -responseFile NO_VALUE         \
+     -characterSet AL32UTF8                                                    \
+     -sysPassword Dimi_prueba_01                                               \
+     -systemPassword Prueba_dimi_01                                            \
+     -createAsContainerDatabase true                                           \
+     -numberOfPDBs 1                                                           \
+     -pdbName ${PDB_NAME}                                                      \
+     -pdbAdminPassword Dimi_pdb_prueba_01                                      \
+     -databaseType MULTIPURPOSE                                                \
+     -memoryMgmtType auto_sga                                                  \
+     -totalMemory 2000                                                         \
+     -storageType FS                                                           \
+     -datafileDestination "${DATA_DIR}"                                        \
+     -redoLogFileSize 50                                                       \
+     -emConfiguration NONE                                                     \
+     -ignorePreReqs
+
+echo "Finished creating database ${ORACLE_SID}
+"
